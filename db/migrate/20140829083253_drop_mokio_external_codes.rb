824b9ea5f1b3842f4467fbb2dab9ed12babe354f
mateusz <mateusz.zoladek@goodonegroup.pl>
2014-09-01 08:28:39 +0200

    Removed migrations

--- db/migrate/20140829083253_drop_mokio_external_codes.rb
+++
@@ -1,7 +0,0 @@
-class DropMokioExternalCodes < ActiveRecord::Migration
-  def change
-    if ActiveRecord::Base.connection.table_exists? :mokio_external_codes
-    drop_table :mokio_external_codes
-    end
-  end
-end
