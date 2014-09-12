70e4164819cf32dc702a59e0f662c3baac9b2481
mateusz <mateusz.zoladek@goodonegroup.pl>
2014-09-12 09:02:35 +0200

    Mokio Simple Logger - lib

---
+++ lib/mokio/mokio_logger.rb
@@ -0,0 +1,25 @@
+class MokioLogger 
+  # new logger object 
+  @@mokiologger = Logger.new("log/#{Rails.env}_mokio.log")
+  #initialize and set debug level
+  def initialize
+     level =  defined?(Rails.configuration.mokio_log_level) ? Rails.configuration.mokio_log_level : Logger::INFO
+     @@mokiologger.level = level
+  end
+  # methods for logged info , error ,debug , warn  msg
+  def info(msg)
+    @@mokiologger.info(msg)
+  end
+
+  def debug(msg) 
+    @@mokiologger.error(msg)
+  end
+
+  def error(msg) 
+    @@mokiologger.debug(msg)
+  end
+  
+  def warn(msg) 
+    @@mokiologger.warn(msg)
+  end
+end
