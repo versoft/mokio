e036b58fd9560bb265d965afaa3bffc5bd2ab262
mateusz <mateusz.zoladek@goodonegroup.pl>
2014-10-27 09:26:51 +0100

    Migrate for external_scripts - external_codes to external_scripts

--- db/migrate/20140731061227_create_mokio_external_scripts.rb
+++ db/migrate/20140731061227_create_mokio_external_scripts.rb
@@ -1,10 +1,12 @@
 class CreateMokioExternalScripts < ActiveRecord::Migration
   def change
-    create_table :mokio_external_scripts do |t|
-      t.text "code"
-      t.string "script"
-      t.boolean "editable",default: true
-      t.boolean "deletable",default: true
+    if !ActiveRecord::Base.connection.table_exists? :mokio_external_scripts
+      create_table :mokio_external_scripts do |t|
+        t.text "code"
+        t.string "script"
+        t.boolean "editable",default: true
+        t.boolean "deletable",default: true
+      end
     end
   end
 end
