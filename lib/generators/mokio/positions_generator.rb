01aa4dee7a2bc86bb4bfbe011bfcde0c44d5e4b7
Tomek <tomasz.krol@goodonegroup.pl>
2014-12-10 10:11:21 +0100

    small fixes

--- lib/generators/mokio/positions_generator.rb
+++ lib/generators/mokio/positions_generator.rb
@@ -1,11 +1,12 @@
 module Mokio
   class PositionsGenerator < Rails::Generators::Base #:nodoc:
     argument :names, :type => :array
-    desc "Creates module positions"
 
+    desc "Creates module positions"
     def create_positions
       names.each do |name|
         Mokio::ModulePosition.create(:name => name)
+        puts "Successfully created #{name.green} position."
       end
     end
 
