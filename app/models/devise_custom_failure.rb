018ce1853f61ac71ca3e6d243795d07a1b452833
MatZet <mateusz.zoladek@bloomnet.eu>
2019-11-25 10:23:54 +0100

    Add login fail communicate and change password fix

--- app/models/devise_custom_failure.rb
+++ app/models/devise_custom_failure.rb
@@ -2,4 +2,13 @@ class DeviseCustomFailure < Devise::FailureApp
   def redirect_url
     '/backend/users/sign_in'
   end
+
+  def respond
+    if http_auth?
+      http_auth
+    else
+      flash[:alert] = I18n.t(:invalid, :scope => [ :devise, :failure ])
+      redirect
+    end
+  end
 end
