cb4f22d80ad89503363b92c24432bcba647268b1
MatZet <mateusz.zoladek@bloomnet.eu>
2019-12-05 15:00:01 +0100

    Removed migrations - base content

--- db/migrate/20150408075454_add_base_content_id_to_mokio_contents.mokio.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from mokio (originally 20150408075347)
-class AddBaseContentIdToMokioContents < ActiveRecord::Migration[5.0]
-  def change
-    add_column :mokio_contents, :base_content_id, :int
-  end
-end
--- db/migrate/20150528104851_add_base_content_id_to_mokio_data_files.rb
+++
@@ -1,5 +0,0 @@
-class AddBaseContentIdToMokioDataFiles < ActiveRecord::Migration[5.0]
-  def change
-    add_column :mokio_data_files, :base_content_id,:integer
-  end
-end
