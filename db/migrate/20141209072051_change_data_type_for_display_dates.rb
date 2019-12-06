40f14b93ecfbd06eed09addbbde77669cbdce481
MatZet <mateusz.zoladek@bloomnet.eu>
2019-12-06 16:48:36 +0100

    Migration all in one

--- db/migrate/20141209072051_change_data_type_for_display_dates.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDataTypeForDisplayDates < ActiveRecord::Migration[5.0]
-  def change
-    change_table :mokio_contents do |t|
-      t.change :display_from, :datetime
-      t.change :display_to, :datetime
-    end
-  end
-end
--- db/migrate/20150414070406_add_author_and_editor_to_mokio_content.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorAndEditorToMokioContent < ActiveRecord::Migration[5.0]
-  def change
-    add_column :mokio_contents, :created_by, :integer, after: :created_at
-    add_column :mokio_contents, :updated_by, :integer, after: :updated_at
-  end
-end
