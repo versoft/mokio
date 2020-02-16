dd6f580b69855d1fa8faa601fde2821a05eac683
Justyna Zelman <justyna.zelman@bloomnet.eu>
2020-02-16 22:38:52 +0100

    Invalidate session cookie after logging out

---
+++ config/initializers/sessions.rb
@@ -0,0 +1,10 @@
+Rails.configuration.to_prepare do
+  Devise::SessionsController.class_eval do
+    before_action :invalidate_current_user_session, only: [:destroy]
+
+    private
+      def invalidate_current_user_session
+        current_user.invalidate_all_sessions!
+      end  
+  end
+end
