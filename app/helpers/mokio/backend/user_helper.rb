8563dbd30ca4d2b1b163cd7aa9ed8a9c0f055547
Justyna Zelman <justyna.zelman@bloomnet.eu>
2020-03-26 13:24:38 +0100

    Disable pass change in Menus

--- app/helpers/mokio/backend/user_helper.rb
+++ app/helpers/mokio/backend/user_helper.rb
@@ -8,6 +8,10 @@ module Mokio
       def can_edit_password_without_confirmation?(obj)
         current_user.is_super_admin? && obj != current_user
       end
+
+      def is_action_user_edit?
+        params[:action] == "edit" && params[:controller] == "users"
+      end
     end
   end
 end
