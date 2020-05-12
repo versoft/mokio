0822152af14b4e9ce6cba52b9f02c952a13c9f96
Justyna Zelman <justyna.zelman@bloomnet.eu>
2020-05-12 15:16:54 +0200

    Add Recipient specs

--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -1,18 +1,36 @@
-# == Schema Information
-#
-# Table name: recipients
-#
-#  id         :integer          not null, primary key
-#  email      :string(255)      not null
-#  active     :boolean          default(TRUE)
-#  created_at :datetime
-#  updated_at :datetime
-#  contact_id :integer
-#
+# frozen_string_literal: true
 
 require 'spec_helper'
+
 module Mokio
-  describe Recipient do
-    pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe Recipient, type: :model do
+    before do
+      @contact = Contact.create!(title: 'test contact')
+      @recipient = Recipient.new(
+        email: 'recipient@example.com',
+        contact_id: @contact.id
+      )
+    end
+
+    it 'is valid with and email and a contact' do
+      @recipient.valid?
+      expect(@recipient).to be_valid
+    end
+
+    it 'is invalid without an email' do
+      @recipient.email = nil
+      @recipient.valid?
+      expect(@recipient.errors[:email]).to include(
+        'Proszę podać adres e-mail w poprawnym formacie'
+      )
+    end
+
+    it 'is invalid in email in a wrong format' do
+      @recipient.email = 'wrong format'
+      @recipient.valid?
+      expect(@recipient.errors[:email]).to include(
+        'Proszę podać adres e-mail w poprawnym formacie'
+      )
+    end
   end
 end
