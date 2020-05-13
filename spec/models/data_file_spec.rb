3e7d5f41f47b45f34567eb68748e22eef77005c9
Justyna Zelman <justyna.zelman@bloomnet.eu>
2020-05-13 10:12:20 +0200

    wip

--- spec/models/data_file_spec.rb
+++ spec/models/data_file_spec.rb
@@ -1,29 +1,13 @@
-# == Schema Information
-#
-# Table name: data_files
-#
-#  id                 :integer          not null, primary key
-#  name               :string(255)
-#  data_file          :string(255)
-#  download_count     :integer          default(0)
-#  seq                :integer
-#  type               :string(255)
-#  active             :boolean          default(TRUE)
-#  movie_url          :string(255)
-#  external_link      :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  content_id         :integer
-#  thumb              :string(255)      default("0")
-#  intro              :text
-#  subtitle           :string(255)
-#  thumb_external_url :string(255)
-#  slug               :string(255)
-#
+# frozen_string_literal: true
 
 require 'spec_helper'
+
 module Mokio
   describe DataFile do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'is valid' do
+      datafile = DataFile.create!
+      datafile.valid?
+      expect(data_file).to be_valid
+    end
   end
 end
